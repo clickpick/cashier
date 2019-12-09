ebee1070af6ef147ff35f5ae10252fd2e74fc32a
Nikita Barinov <nikitabarinov2301@gmail.com>
2019-12-09 21:50:25 +0400

    add AddGroup Component from Payment View

--- src/views/Payment/Payment.js
+++ src/views/Payment/Payment.js
@@ -1,17 +1,21 @@
-import React, { useCallback, useEffect } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import { string } from 'prop-types';
 
 import { useSelector, useDispatch } from 'react-redux';
 import { getPaymentState } from 'reducers/payment-reducer';
 import { goForward, goBack } from 'actions/payment-actions';
 
+import { getUserState } from 'reducers/user-reducer';
+import { fetchAttachGroup } from 'actions/user-actions';
+
 import connect from '@vkontakte/vk-connect';
 
-import { ConfigProvider, View } from '@vkontakte/vkui';
+import { ConfigProvider, View, ScreenSpinner } from '@vkontakte/vkui';
 
 import Home from 'panels/Home';
-
 import * as PANELS from 'constants/panels';
+
+import AddGroup from 'components/AddGroup';
 
 const callback = (prevPanel, nextPanel) => {
     if (prevPanel === PANELS.HOME) {
@@ -23,6 +27,11 @@ const callback = (prevPanel, nextPanel) => {
 
 const Payment = ({ id }) => {
     const viewState = useSelector(getPaymentState);
+    const [popout, setPopout] = useState(<ScreenSpinner />);
+
+    const { loading, ownedGroups, cashiedGroups } = useSelector(getUserState);
+
+    const [showAddGroup, setShowAddGroup] = useState(false);
 
     const dispatch = useDispatch();
 
@@ -36,6 +45,14 @@ const Payment = ({ id }) => {
     }, [dispatch]);
 
     const back = useCallback(() => dispatch(goBack), [dispatch]);
+
+    const onAttachGroup = useCallback((groupId, accessToken) => {
+        const result = dispatch(fetchAttachGroup(groupId, accessToken));
+        
+        if (result) {
+            setShowAddGroup(false);
+        }
+    }, [dispatch]);
 
     useEffect(() => {
         function handlePopState(e) {
@@ -62,13 +79,29 @@ const Payment = ({ id }) => {
         };
     }, [back]);
 
-    return (
+    useEffect(() => {
+        if (!loading) {
+            setPopout(null);
+
+            if (Array.isArray(ownedGroups) && Array.isArray(cashiedGroups) &&
+                ownedGroups.length === 0 && cashiedGroups.length === 0) {
+                setShowAddGroup(true);
+            }
+        }
+    }, [loading, ownedGroups, cashiedGroups]);
+
+    return <>
         <ConfigProvider isWebView={true}>
-            <View id={id} {...viewState} onSwipeBack={back}>
+            <View id={id} {...viewState} popout={popout} onSwipeBack={back}>
                 <Home id={PANELS.HOME} />
             </View>
         </ConfigProvider>
-    );
+
+        <AddGroup
+            visible={showAddGroup}
+            disabled={true}
+            attachGroup={onAttachGroup} />
+    </>;
 };
 
 Payment.propTypes = {
